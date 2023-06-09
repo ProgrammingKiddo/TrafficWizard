0d4d71421c22f6e747c9be7164bf9361b0430feb
ProgrammingKiddo <programming.edu@protonmail.com>
2023-06-09 17:20:12 +0200

    Tested interaction with target models OK.

---
+++ IndomitusReader/Assets/MyAssets/Scripts/ButtonCommand.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ButtonCommand : MonoBehaviour
+{
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (this.GetComponent<AudioSource>())
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
