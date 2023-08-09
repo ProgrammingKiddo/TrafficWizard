9eba76c5c7ed8fbd2cc86f4875c866b9bdaff072
ProgrammingKiddo <programming.edu@protonmail.com>
2023-08-09 22:42:01 +0200

    Added ScriptableObjects.

---
+++ IndomitusReader/Assets/MyAssets/Scripts/TrafficSign.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [CreateAssetMenu(fileName = "New_TrafficSign", menuName = "New Traffic Sign")]
+public class TrafficSign : ScriptableObject {
+
+    public string Name;
+    public string Text;
+    public Sprite Sign;
+}
